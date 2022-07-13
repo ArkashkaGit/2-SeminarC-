81720d8ccfafecd353cb1df222221751c59a6632
Arkashka <sevenkapsul@mail.ru>
2022-07-13 20:08:48 +0300

    2_Zadacha

--- Job/13/Program.cs
+++ Job/13/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 13: 
+Напишите программу, 
+которая выводит третью цифру заданного числа 
+или сообщает, что третьей цифры нет.
+
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+Console.WriteLine("Введите число...");
+int num = int.Parse(Console.ReadLine());
+
+
+if (num < 100)
+{
+    Console.WriteLine("третьей цыфры нет...");
+}
+else
+{
+    while (num > 999)
+    {
+        num = num / 10;
+    }
+    if (num < 999)
+    {
+        num = num % 10;
+        Console.WriteLine("Третья цыфра заданного числа равна..\n" + num);
+    }
+}
